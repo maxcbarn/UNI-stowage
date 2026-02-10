71cbd66d517b55e73aeae8577b3e2077e29be97d
Eduardo-Machado-Behling <eduardomachadobehling@hotmail.com>
2026-02-09 21:23:36 -0300

    [Merge]: recovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from common import *
 from exact import *
-from aproximative.FFD import *
 from genetic import *
 
 # fazer test cases
@@ -20,6 +19,7 @@ from genetic import *
 
 # 10 X 10 X 10 = 1000 - 1000
 
+
 def main():
     # Example containers (id, weight, dest)
     containers = [
@@ -35,63 +35,20 @@ def main():
         {'id': 10, 'weight': 8.0, 'dest': 1},
     ]
 
-<<<<<<< HEAD
-    num_bays = 20
-    num_rows = 10
-    max_tiers = 5
-
-    # You can change ALPHA/BETA to tune priority between rehandles and balance
-    res = solve_stowage_3d_min_rehandles_with_cog(
-        ContainerRamdom(60), num_bays, num_rows, max_tiers,
-        ALPHA=1.0, BETA=0.05
-    )
-
-    print(res)
-
-
-main()
-=======
     num_bays = 5
     num_rows = 2
     max_tiers = 8
 
-    containerR = ContainerRamdom(10);
-    print( containerR )
+    containerR = ContainerRamdom(10)
+    print(containerR)
     # You can change ALPHA/BETA to tune priority between rehandles and balance
     res = solve_stowage_3d_min_rehandles_with_cog(
         containerR, num_bays, num_rows, max_tiers,
         ALPHA=1.0, BETA=0.05
-    ) 
-    print( res );
+    )
+    print(res)
     res = solve_stowage_genetic(containerR, num_bays, num_rows, max_tiers)
-    print( res );
-    
-    
+    print(res)
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 main()
->>>>>>> 0273d8c942d17fb296bd7c4f01970a4d60050bba
